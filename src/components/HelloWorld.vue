91f60b33f35c42a62325dcee1bf8fd2d5317f8cc
J0HN_lnwza <kodchanat2@gmail.com>
2025-10-14 10:40:58 +0700

    chore(tailwind): init tailwind v4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,9 +9,9 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+  <div class="card flex flex-col items-center justify-center">
+    <button type="button" class="bg-blue-500 text-white p-2 rounded-md" @click="count++">count is {{ count }}</button>
+    <p class="text-sm text-gray-500">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
