f247d3172906623f99a873f5c0e9c02174987df7
J0HN_lnwza <kodchanat2@gmail.com>
2025-10-16 12:23:44 +0700

    perf(toast): add vue3-toastify

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from '@/router'
 import i18n from '@/locales'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+const toastifyOptions: ToastContainerOptions = {
+  // type: "default",
+  // position: "top-right",
+  theme: "auto",
+  pauseOnFocusLoss: false,
+  dangerouslyHTMLString: true
+};
 
 createApp(App)
   .use(createPinia())
   .use(router)
   .use(i18n)
+  .use(Vue3Toastify, toastifyOptions)
   .mount('#app')
