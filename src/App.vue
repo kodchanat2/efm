a4916f5de9a02d17c7190f4974fb4faec2fd3ec0
J0HN_lnwza <kodchanat2@gmail.com>
2025-10-17 15:59:11 +0700

    style(loading): styling loading msg

--- src/App.vue
+++ src/App.vue
@@ -26,11 +26,11 @@ onMounted(() => {
       </RouterLink>
     </template>
     </nav> -->
-    <div v-if="loading">
+    <div v-if="loading" class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold text-muted animate-pulse">
       {{ $t('common.loading') }}
     </div>
     <!-- Error State -->
-    <div v-else-if="error" class="text-destructive mb-4">
+    <div v-else-if="error" class="text-destructive mt-4 mx-auto text-center">
       {{ error.message }}
     </div>
     <RouterView v-else/>
